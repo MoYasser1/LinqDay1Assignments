22edc6d341555afae5566b5acaeb67a56709e4fb
moham <moham@DIVMOASHOR>
2024-10-29 00:26:01 +0300

    Com

--- LinqDay1Assignments/Program.cs
+++ LinqDay1Assignments/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Xml.Linq;
 
 namespace LinqDay1Assignments
 {
@@ -6,14 +8,12 @@ namespace LinqDay1Assignments
     {
         static void Main(string[] args)
         {
-            //Declare a List of numbers as shown :   
+            //1 -Declare a List of numbers as shown :   
             //List<int> numbers = new List<int>() { 2, 4, 6, 7, 1, 4, 2, 9, 1 };
             //Then write the following queries
             #region Display numbers without any repeated Data and sorted  
             //List<int> numbers = new List<int>() { 2, 4, 6, 7, 1, 4, 2, 9, 1 };
-
             //var query1 = numbers.Distinct().OrderBy(n => n);
-
             //foreach (var number in query1)
             //{
             //    Console.WriteLine(number);
@@ -23,14 +23,12 @@ namespace LinqDay1Assignments
             //List<int> numbers = new List<int>() { 2, 4, 6, 7, 1, 4, 2, 9, 1 };
             //var query1 = numbers.Distinct().OrderBy(n => n);
             //var query2 = query1.Select(n => new { Number = n, Multiplication = n * n });
-
             //foreach (var item in query2)
             //{
             //    Console.WriteLine($"Number: {item.Number}, Multiplication: {item.Multiplication}");
             //}
-
             #endregion
-            //declare an array of names as shown :   
+            //2-declare an array of names as shown :   
             //string[] names = { "Tom", "Dick", "Harry", "MARY", "Jay" };
             #region Query1: Select names with length equal 3. o / p should be as
             //        string[] names = { "Tom", "Dick", "Harry", "MARY", "Jay" };
@@ -54,8 +52,94 @@ namespace LinqDay1Assignments
             //    Console.WriteLine(name);
             //} 
             #endregion
+            #region Query3: Display the first 2 names  
+            //string[] names = { "Tom", "Dick", "Harry", "MARY", "Jay" };
+            //Console.WriteLine(names[0]);
+            //Console.WriteLine(names[1]); 
+            #endregion
+            //3 - Declare a class Subject that contains the following properties(Code, Name) and declare Student class 
+            //that contains the following properties(ID, FirstName, LastName , Subject[]), then define List of students As
+            //follow
+            #region Query1: Display Full name and number of subjects for each student as follow  
+
+            //List<Student> students = new List<Student>()
+            //    new Student()
+            //    {
+            //        ID = 1,
+            //        FirstName = "Mohamed",
+            //        LastName = "Yasser",
+            //        Subjects = new Subject[]
+            //        {
+            //            new Subject(){ Code = 22, Name = "EF" },
+            //            new Subject(){ Code = 33, Name = "UML" }
+            //        }
+            //    },
+            //    new Student()
+            //    {
+            //        ID = 2,
+            //        FirstName = "Mona",
+            //        LastName = "Gala",
+            //        Subjects = new Subject[]
+            //        {
+            //            new Subject(){ Code = 22, Name = "EF" },
+            //            new Subject(){ Code = 34, Name = "XML" },
+            //            new Subject(){ Code = 25, Name = "JS" }
+            //        }
+            //    },
+            //    new Student()
+            //    {
+            //        ID = 3,
+            //        FirstName = "Yara",
+            //        LastName = "Yousf",
+            //        Subjects = new Subject[]
+            //        {
+            //            new Subject(){ Code = 22, Name = "EF" },
+            //            new Subject(){ Code = 25, Name = "JS" }
+            //        }
+            //    },
+            //    new Student()
+            //    {
+            //        ID = 4,
+            //        FirstName = "Ali",
+            //        LastName = "Ali",
+            //        Subjects = new Subject[]
+            //        {
+            //            new Subject(){ Code = 33, Name = "UML" }
+            //        }
+            //    }
+            //};
+            //            var query1 = students.Select(s => new
+            //            {
+            //                FullName = $"{s.FirstName} {s.LastName}",
+            //                NumberOfSubjects = s.Subjects.Length
+            //            });
+
+            //            foreach (var student in query1)
+            //            {
+            //                Console.WriteLine($"Full Name: {student.FullName}, Number of Subjects: {student.NumberOfSubjects}");
+            //            }
 
 
+            #endregion
+            #region Query2: Write a query which orders the elements in the list by FirstName Descending then by LastName Ascending and result of query displays only first names and last names for the elements in list as follow  
+
+
+            //    var query2 = students
+            //.OrderByDescending(s => s.FirstName)
+            //.ThenBy(s => s.LastName)
+            //.Select(s => new
+            //{
+            //    s.FirstName,
+            //    s.LastName
+            //});
+
+            //        foreach (var student in query2)
+            //        foreach (var student in query2)
+            //        {
+            //            Console.WriteLine($"First Name: {student.FirstName}, Last Name: {student.LastName}");
+            //        }
+
+            #endregion
         }
     }
 }
---
+++ LinqDay1Assignments/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDay1Assignments
+{
+    internal class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Subject[] Subjects { get; set; }
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
